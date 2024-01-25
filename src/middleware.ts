1a006173e09e7cc39b27a8cdfd2f06fcd419e6ee
Fur619 <furqanarshad98.fa@gmail.com>
2024-01-25 22:30:06 +0500

    redis rate limitation and middelware

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+import { rateLimiter } from "./lib/rate-limiter";
+
+export async function middleware(req: NextRequest) {
+    const ip = req.ip ?? "127.0.0.1"
+    try {
+        const { success } = await rateLimiter.limit(ip)
+        if (!success) return new NextResponse("You are chatting to fast!")
+    } catch (error) {
+        return new NextResponse("Sorry, something went wrong, try again later")
+    }
+}
+
+export const config = {
+    matcher: "/api/message/:path"
+}
