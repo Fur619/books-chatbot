e3da13dee3d69de235aba54c3e904b0fdd0a8bd2
Fur619 <furqanarshad98.fa@gmail.com>
2024-01-25 22:41:53 +0500

    Update main page UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,60 @@
-import Image from "next/image";
+'use client'
 
 export default function Home() {
   return (
-    <main className="absolute inset-0 flex justify-center items-center">
-     website content
-    </main>
-  );
+    <div className='bg-white'>
+      <header className='absolute inset-x-0 top-0 z-50'>
+        <nav
+          className='flex items-center justify-between p-6 lg:px-8'
+          aria-label='Global'>
+          <div className='flex lg:flex-1'>
+            <a href='#' className='-m-1.5 p-1.5'>
+              <span className='sr-only'>Your Company</span>
+              <img
+                className='h-8 w-auto'
+                src='/book.png'
+                alt=''
+              />
+            </a>
+          </div>
+        </nav>
+      </header>
+
+      <div className='relative isolate pt-14'>
+        <div
+          className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
+          aria-hidden='true'>
+          <div
+            className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
+        <div className='py-24 sm:py-32 lg:pb-40'>
+          <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+            <div className='mx-auto max-w-2xl text-center'>
+              <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+                Your go-to bookstore chatbot for fantasy and mystery
+              </h1>
+              <p className='mt-6 text-lg leading-8 text-gray-600'>
+                FQ Bookbuddy is a small bookstore that specializes in fantasy and mystery
+                books. Use chatbot to find books in store.
+              </p>
+            </div>
+            <div className='mt-16 flow-root sm:mt-24'>
+              <div className='relative -m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                <img
+                  src='/bookstore2.jpg'
+                  alt='App screenshot'
+                  className='rounded-md shadow-2xl ring-1 ring-gray-900/10'
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
